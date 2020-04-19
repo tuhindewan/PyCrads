f87ad982c5f3c127abafb5440976911c291331d8
tuhindewan <tuhinsshadow@gmail.com>
2020-04-19 21:51:18 +0600

    Declaration and Defination of Deck Class

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Card:
     def __init__(self, suit, val):
         self.suit = suit
@@ -7,5 +10,29 @@ class Card:
         print(f"{self.value} of {self.suit}")
 
 
-card = Card("Clubs", 3)
-card.show()
+class Deck:
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for suit in ['Spades', 'Clubs', 'Diamonds', 'Hearts']:
+            for value in range(1, 14):
+                self.cards.append(Card(suit, value))
+
+    def show(self):
+        for card in self.cards:
+            card.show()
+
+    def shuffle(self):
+        for i in range(len(self.cards) - 1, 0, -1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+
+    def draw(self):
+        return self.cards.pop()
+
+
+deck = Deck()
+deck.shuffle()
+deck.show()
